cda4fe88abc71b16041f7aff7b231df998a77ba9
Aneesh-382005 <aneesh382005@gmail.com>
2024-11-14 02:57:37 +0530

    Add or update files as needed

--- Singly Linked Lists/Reverse.java
+++ Singly Linked Lists/Reverse.java
@@ -12,5 +12,19 @@ public class Reverse extends LinkedLists
             prev = current;
             current = next;
         }
+        head = prev;
+    }
+
+    public static void main(String[] args)
+    {
+        Reverse list = new Reverse();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.display();
+        list.ReverseList();
+        list.display();
     }
 }
